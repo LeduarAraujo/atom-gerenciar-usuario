aeaeaf8006e68af3e1bc9afdc26d56cdc5c8dfb2
leduararaujo <leduar.araujo@hotmail.com>
2023-08-26 12:33:23 -0300

    2 Implementação da geração das apis pelo swagger.

---
+++ src/main/java/com/leduar/atomgerenciarusuario/enums/ExceptionReasonEnum.java
@@ -0,0 +1,27 @@
+package com.leduar.atomgerenciarusuario.enums;
+
+import com.leduar.atomgerenciarusuario.exceptions.LoginSenhaException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+
+@AllArgsConstructor
+@Getter
+public enum ExceptionReasonEnum {
+    LOGIN_INEXISTENTE(LoginSenhaException.class, HttpStatus.UNPROCESSABLE_ENTITY, "Invalid login or password");
+
+    private Class exception;
+    private HttpStatus statusCode;
+    private String dsError;
+
+    public static ExceptionReasonEnum getEnum(Class param) {
+        for (ExceptionReasonEnum valor : values()) {
+            if (valor.exception == param) {
+                return valor;
+            }
+        }
+        return LOGIN_INEXISTENTE;
+    }
+}
---
+++ src/main/java/com/leduar/atomgerenciarusuario/rest/GerenciarUsuarioController.java
@@ -0,0 +1,83 @@
+package com.leduar.atomgerenciarusuario.rest;
+
+import com.baeldung.openapi.api.ApiApi;
+import com.baeldung.openapi.model.*;
+import com.leduar.atomgerenciarusuario.service.GerenciarUsuarioService;
+import com.leduar.atomgerenciarusuario.utils.ErrorFormat;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GerenciarUsuarioController implements ApiApi {
+
+    private final GerenciarUsuarioService service;
+
+    @Override
+    public ResponseEntity<SigninUsuarioResponseRepresentation> signin(SigninUsuarioRequestRepresentation signinUsuarioRequestRepresentation) {
+        try {
+            return ResponseEntity.ok().body(service.iniciarSessao(signinUsuarioRequestRepresentation));
+        } catch (Exception ex) {
+            return (ResponseEntity) ErrorFormat.convertToEntity(ex);
+        }
+    }
+
+    @Override
+    public ResponseEntity<ListUsersResponseRepresentation> listUsers() {
+        try {
+            return ResponseEntity.ok().body(null);
+        } catch (Exception ex) {
+            return (ResponseEntity) ErrorFormat.convertToEntity(ex);
+        }
+    }
+
+    @Override
+    public ResponseEntity<SucessMessageRepresentation> cadUsers(DadosUsuarioResponseRepresentation body) {
+        try {
+            return ResponseEntity.ok().body(null);
+        } catch (Exception ex) {
+            return (ResponseEntity) ErrorFormat.convertToEntity(ex);
+        }
+    }
+
+    @Override
+    public ResponseEntity<DadosUsuarioResponseRepresentation> buscarUsuario(Long id) {
+        try {
+            return ResponseEntity.ok().body(null);
+        } catch (Exception ex) {
+            return (ResponseEntity) ErrorFormat.convertToEntity(ex);
+        }
+    }
+
+    @Override
+    public ResponseEntity<SucessMessageRepresentation> removerUsuario(Long id) {
+        try {
+            return ResponseEntity.ok().body(null);
+        } catch (Exception ex) {
+            return (ResponseEntity) ErrorFormat.convertToEntity(ex);
+        }
+    }
+
+    @Override
+    public ResponseEntity<SucessMessageRepresentation> atualizarUsuario(Long id, DadosUsuarioResponseRepresentation body) {
+        try {
+            return ResponseEntity.ok().body(null);
+        } catch (Exception ex) {
+            return (ResponseEntity) ErrorFormat.convertToEntity(ex);
+        }
+    }
+
+    /*
+    * - Endpoints que requer Autenticação
+    */
+
+    @Override
+    public ResponseEntity<GetUsuarioLogadoResponseRepresentation> getUsuarioLogado(String tokenJwt) {
+        try {
+            return ResponseEntity.ok().body(service.getDadosUsuario(tokenJwt));
+        } catch (Exception ex) {
+            return (ResponseEntity) ErrorFormat.convertToEntity(ex);
+        }
+    }
+}
