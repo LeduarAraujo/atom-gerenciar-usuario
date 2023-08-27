fe1e343d019dbbe925575f29a018bf6b45650dc2
leduararaujo <leduar.araujo@hotmail.com>
2023-08-26 22:07:48 -0300

    #11 Implementacao da api de atualização de dados do usuario.

--- src/main/java/com/leduar/atomgerenciarusuario/repository/UsuarioRepository.java
+++ src/main/java/com/leduar/atomgerenciarusuario/repository/UsuarioRepository.java
@@ -14,8 +14,8 @@ public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 
     List<UsuarioEntity> findUsuarioEntityByEmail(String email);
 
-    List<UsuarioEntity> findUsuarioEntityByLoginAndIdIsNot(String login, Long id);
+    List<UsuarioEntity> findUsuarioEntityByLoginAndIdNot(String login, Long id);
 
-    List<UsuarioEntity> findUsuarioEntityByEmailAndIdIsNot(String email, Long id);
+    List<UsuarioEntity> findUsuarioEntityByEmailAndIdNot(String email, Long id);
 
 }
